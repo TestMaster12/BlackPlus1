7925392893cde070127152dd3cd8a4c5efdcb34f
TeleMrjX <mr.jalali76@gmail.com>
2016-04-09 14:13:01 +0000

    Update inPm.lua

--- plugins/inPm.lua
+++ plugins/inPm.lua
@@ -90,8 +90,8 @@ if to == 'user' or service or is_admin1(msg) and to == "chat" or to == "channel"
 	end
     if matches[1] == 'join' then
 	local data = load_data(_config.moderation.data)
-	if matches[2]:lower() == 'blackplus' and matches[3]:lower() == 'support' then
-		savelog(msg.to.id, name_log.." ["..msg.from.id.."] tried to join blackplus support")
+	if matches[2]:lower() == 'megatesla' and matches[3]:lower() == 'support' then
+		savelog(msg.to.id, name_log.." ["..msg.from.id.."] tried to join megatesla support")
 		local target = 1031459611
 		local long_id = data[tostring(target)]['long_id']
 		if is_banned(msg.from.id, tostring(target)) then
@@ -208,7 +208,7 @@ if to == 'user' or service or is_admin1(msg) and to == "chat" or to == "channel"
 
 	if matches[1] == 'help' and msg.to.type == 'user' or matches[1] == 'pmhelp' and is_admin1(msg) and msg.to.type ~= 'user' then
       	savelog(msg.to.id, name_log.." ["..msg.from.id.."] used pm help")
-		text = "Welcome to BlackPlus!\n\n>To get a new BlackPlus group, contact a support group:\n https://telegram.me/joinchat/A2ETvD160xvoj4y_pNAgig\n\nFor more information, check out our channels:\n\n@Black_CH \n\nUse #superhelp command to show bot commands!!\n\n#Thanks_for_using @BlackPlus!"
+		text = "Welcome to MegaTesla!\n\n>To get a new MegaTesla group, contact a support group:\nsend #reqgp\n\nFor more information, check out our channels:\n\n@MegaTesla_Ch \n\nUse #superhelp command to show bot commands!!\n\n#Thanks_for_using @MegaTesla!"
      	return text
     end
 
