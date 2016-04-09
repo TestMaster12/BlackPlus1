ccee456698318886f17e07c7340a49b94726971e
TeleMrjX <mr.jalali76@gmail.com>
2016-04-09 14:20:09 +0000

    Update sudoers.lua

--- plugins/sudoers.lua
+++ plugins/sudoers.lua
@@ -4,9 +4,9 @@ function run(msg, matches)
   return [[
 #Sudoers!
   
-  1.@MehdiHS🌐 #Developer ( use #adddeveloper command to add #Mehdi_HS in your Groups & SuperGroups. )
+  1.@The_DeSigN3r🌐 #Developer ( use #adddeveloper command to add #The_DeSigN3r in your Groups & SuperGroups. )
   
-  2.@YflQw🌐 #Manager ( use #addmanager command to add #Vandad_Yflqw in your Groups & SuperGroups. )
+  2.@ParsaAlemi🌐 #Manager ( use #addmanager command to add #ParsaAlemi in your Groups & SuperGroups. )
   
 ]]
 end
