32f728e014d55ae351a2d5f0575394bef3b04d19
TestMaster12 <aryapc320@yahoo.com>
2016-04-15 04:45:45 +0000

    Update groupRequest_V2_Test.lua

--- plugins/groupRequest_V2_Test.lua
+++ plugins/groupRequest_V2_Test.lua
@@ -1,16 +1,17 @@
 do
 
  function run(msg, matches)
- local ch = '1040275374'..msg.to.id
+ local ch = '1040778813'..msg.to.id
  local fuse = 'New SuperGroup Request!\n\nId : ' .. msg.from.id .. '\n\nName: ' .. msg.from.print_name ..'\n\nUsername: @' .. msg.from.username ..'\n\nMessage From: '..msg.to.id.. '\n\nThe Pm:\n' .. matches[1]
  local fuses = '!printf user#id' .. msg.from.id
 
 
    local text = matches[1]
-   local chat = "channel#id"..1040275374
+   local chat = "channel#id"..1040778813
 
   local sends = send_msg(chat, fuse, ok_cb, false)
-  return '*Request Is Sent!\nYour SuperGroup Is Waiting For Create!'
+  return '*Request Is Sent!\nYour SuperGroup Is Waiting For Create!\nبا تشکر!درخواست سوپرگروپ شما با موفیت ارسال شد\nلطفا برای ساخته شدن سوپرگروپ صبر کنید!'
+  
 
  end
  end
