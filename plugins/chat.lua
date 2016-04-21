8e5c86c8a59bda948e78b61a56acfdae46b4a8be
TestMaster12 <aryapc320@yahoo.com>
2016-04-21 11:04:58 +0000

    Update chat.lua

--- plugins/chat.lua
+++ plugins/chat.lua
@@ -1,24 +1,16 @@
-do
+local function run(msg)
+    if msg.text == "سلام" then
+    if is_sudo(msg) then
+        return "سلام بابایی!"
+    end
+end
 
-function run(msg, matches)
-local reply_id = msg['id']
-local text = 'سلام بابایی جونم😍'
-local m = '87946267'
-if matches[1] == 'salam' or 'سلام' or 'slm' or 'i' then
-if result.id = tonumber(m) then
-    text = 'test'
-reply_msg(reply_id, text, ok_cb, false)
-end
-end 
-end
 return {
-patterns = {
-    "^salam$",
-    "^سلام$",
-    "^slm$",
-    "^[Hh]i"
-},
-run = run
-}
-
-end
+    description = "chatting",
+    usage = "chat"
+    patterns = {
+        "سلام",
+        },
+    run = run,
+    pre_process = pre_process
+        }
