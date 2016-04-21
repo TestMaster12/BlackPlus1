a68ddf86401297f5534c5d2c0cfa54bf08250073
TestMaster12 <aryapc320@yahoo.com>
2016-04-21 11:24:23 +0000

    Update chat.lua

--- plugins/chat.lua
+++ plugins/chat.lua
@@ -8,7 +8,7 @@ local text = "Hello Daddy"
 	reply_msg(reply_id, text, ok_cb, false)
 end
 if msg.text == "سلام" and is_sudo(msg) then
-local text = "☺️خسته نباشی،❤️سلام باباجونم"
+local text = "☺️👋سلام بابا جونم❤،️خسته نباشی"
 	reply_msg(reply_id, text, ok_cb, false)
 end
 if msg.text == "hello" then
