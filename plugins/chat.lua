39888c95397dfe69b2ba5edfa0005e7b79babc2d
TestMaster12 <aryapc320@yahoo.com>
2016-04-21 10:56:07 +0000

    Update chat.lua

--- plugins/chat.lua
+++ plugins/chat.lua
@@ -5,7 +5,8 @@ local reply_id = msg['id']
 local text = 'سلام بابایی جونم😍'
 local m = '87946267'
 if matches[1] == 'salam' or 'سلام' or 'slm' or 'i' then
-    if (tonumber) 'm'  then
+if result.id = tonumber(m) then
+    text = 'test'
 reply_msg(reply_id, text, ok_cb, false)
 end
 end 
